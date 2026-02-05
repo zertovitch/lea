2b53c09ec3f5f80e533cb23a6a470119bf1cb6ca
unknown <gdemont@hotmail.com>
2026-02-05 07:19:03 +0100

    RC 0.93

--- lea_resource_gui.adb
+++ lea_resource_gui.adb
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------
 --  GUI contents of resource script file: LEA.rc
---  Transcription time: 2026/01/28  20:03:50
+--  Transcription time: 2026/02/05  07:17:46
 --  GWenerator project file: lea.gwen
 --
 --  Translated by the RC2GW or by the GWenerator tool.
--- lea_resource_gui.ads
+++ lea_resource_gui.ads
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------
 --  GUI contents of resource script file: LEA.rc
---  Transcription time: 2026/01/28  20:03:50
+--  Transcription time: 2026/02/05  07:17:46
 --  GWenerator project file: lea.gwen
 --
 --  Translated by the RC2GW or by the GWenerator tool.
@@ -490,8 +490,8 @@ package LEA_Resource_GUI is
   package Version_info is
     Authors : constant String := "Gautier de Montmollin";
     FileDescription : constant String := "LEA - a Lightweight Editor for Ada - Free, MIT license";
-    FileVersion : constant String := "0.92";
-    LegalCopyright : constant String := "Copyright © Gautier de Montmollin 2017 .. 2025";
+    FileVersion : constant String := "0.93";
+    LegalCopyright : constant String := "Copyright © Gautier de Montmollin 2017 .. 2026";
     ProductName : constant String := "LEA";
     Translation : constant := 1033;
   end Version_info;
